4a490bd7a98d7a712d56d3baa111bb46993a107a
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2023-12-28 03:12:07 +0300

    auth data sending to the server

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,39 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+const { generateUserData } = require('../utils/helpers')
+const tokenService = require('../services/token.service')
 const router = express.Router({ mergeParams: true })
 
 router.post('/signUp', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const exitingUser = await User.findOne({ email })
 
+    if (exitingUser) {
+      return res.status(400).json({
+        error: {
+          message: 'EMAIL_EXISTS',
+          code: 400
+        }
+      })
+    }
+
+    const hashedPassword = bcrypt.hash(password, 12)
+
+    const newUser = User.create({
+      ...req.body,
+      ...generateUserData(),
+      password: hashedPassword,
+    })
+    const tokens = tokenService.generate({ _id: newUser._id })
+
+    res.status(201).send({ ...tokens, userId: newUser._id })
+  } catch (e) {
+    res.status(500).json({
+      message: 'на сервере произошла ошибка. Попробуйте позже.'
+    })
+  }
 })
 
 router.post('/signInWithPassword', async (req, res) => {
