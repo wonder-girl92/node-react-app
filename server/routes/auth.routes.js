475dc8564f9ee9aea542d8a527fd3aeecf1073c1
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-01-14 20:18:28 +0300

    refresh token

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -17,7 +17,7 @@ router.post('/signUp', [
           error: {
             message: 'INVALID_DATA',
             code: 400,
-            // errors: errors.array()
+            errors: errors.array()
           }
         })
       }
@@ -103,8 +103,31 @@ router.post('/signInWithPassword', [
   ]
 )
 
-router.post('/token', async (req, res) => {
+function isTokenInvalid(data, dbToken) {
+  return !data || !dbToken || data._id !== dbToken?.user?.toString()
+}
 
+router.post('/token', async (req, res) => {
+try {
+const {refresh_token: refreshToken} = req.body
+  const data = tokenService.validateRefresh(refreshToken)
+  const dbToken = await tokenService.findToken(refreshToken)
+
+  if(isTokenInvalid(data, dbToken)) {
+    return res.status(401).json({message: 'Unauthorized'})
+  }
+
+  const tokens = await tokenService.generate({
+    _id: data._id
+  })
+await tokenService.save(data._id, tokens.refreshToken)
+
+  res.status(200).send({...tokens, userId: data._id})
+} catch (e) {
+  res.status(500).json({
+    message: 'На сервере произошла ошибка. Попробуйте позже.'
+  })
+}
 })
 
 module.exports = router
