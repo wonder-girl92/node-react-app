97f8e3e796ae538a599714b6e25eaf7a16f7e71b
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-01-18 21:51:24 +0300

    patch & get routers for user

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,33 @@
 const express = require('express')
+const User = require('../models/User')
 const router = express.Router({ mergeParams: true })
 
+router.patch('/:userId', async (req, res) => {
+    try {
+        const {userId} = req.params
+      if (userId) {
+       const updatedUser = await User.findByIdAndUpdate(userId, req.body, {new: true})
+        res.send(updatedUser)
+      } else {
+        res.status(401).json({message: 'Unauthorized'})
+      }
+
+    } catch (e) {
+      res.status(500).json({
+        message: 'На сервере произошла ошибка. Попробуйте позже.'
+      })
+    }
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const list = await User.find()
+    res.send(list)
+  } catch (e) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже.'
+    })
+  }
+})
 
 module.exports = router
