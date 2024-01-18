6f7e92478fb5e0148ca6ec4a6baebbe8bd24503d
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-01-18 23:35:22 +0300

    creating auth middleware

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const User = require('../models/User')
+const auth = require('../middleware/auth.middleware')
 const router = express.Router({ mergeParams: true })
 
-router.patch('/:userId', async (req, res) => {
+router.patch('/:userId', auth, async (req, res) => {
     try {
         const {userId} = req.params
-      if (userId) {
+      if (userId  === req.user._id) {
        const updatedUser = await User.findByIdAndUpdate(userId, req.body, {new: true})
         res.send(updatedUser)
       } else {
@@ -19,7 +20,7 @@ router.patch('/:userId', async (req, res) => {
     }
 })
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
     const list = await User.find()
     res.send(list)
